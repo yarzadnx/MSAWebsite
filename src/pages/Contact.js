595a559ebdeee936b3e5046ce6ab57ce6d6b8682
Naweed Yarzada <yarzadnx@dukes.jmu.edu>
2024-05-05 13:16:13 -0400

    a

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import Notiflix from "notiflix";
 
 const Contact = () => {
-  useDocTitle("MLD | Molad e Konsult - Send us a message");
+  useDocTitle("JMU MSA");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
