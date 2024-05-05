fdc227cb721543441b9d8e6df1c2f46276316b29
hara <koluwasanmi@yahoo.com>
2024-05-05 13:31:21 -0600

    fix portiflo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,96 +1,137 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
-    return (
-        <>
-            <div className="my-4 py-4" id='portfolio'>
-                <h2 className="my-2 text-center text-3xl text-purple-900 uppercase font-bold">Activities</h2>
-                <div className='flex justify-center'>
-                    <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
+  return (
+    <>
+      <div className="my-4 py-4" id="portfolio">
+        <h2 className="my-2 text-center text-3xl text-purple-900 uppercase font-bold">
+          Activities
+        </h2>
+        <div className="flex justify-center">
+          <div className="w-24 border-b-4 border-blue-900 mb-8"></div>
+        </div>
+
+        <div className="px-4" data-aos="fade-down" data-aos-delay="600">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
+            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
+              <div className="m-2 text-justify text-sm">
+                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
+                  Weekly Meetings
+                </h4>
+                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                  Join us every Tuesday at 7 PM for our engaging JEM (JMU
+                  Engaging Muslims) meetings, where we bring together a vibrant
+                  community for an enriching experience. Our gatherings feature
+                  captivating speakers, enlightening country showcases, and
+                  interactive Islamic trivia sessions. Each week, we delve into
+                  topics that celebrate our culture, faith, and shared
+                  experiences, fostering a warm atmosphere of learning and
+                  connection.
+                </p>
+                <div className="flex justify-center my-4">
+                  <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                           7pm: be There   </Link>
                 </div>
-
-                <div className="px-4" data-aos="fade-down" data-aos-delay="600">
-                    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">                            
-                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
-                            <div className="m-2 text-justify text-sm">
-                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Weekly Meetings</h4>
-                                <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                    hjhj
-                                </p>
-                                <div className="flex justify-center my-4">
-                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
-                                        Schedule Demo
-                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
-                            <div className="m-2 text-justify text-sm">
-                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">School Management Portal</h4>
-                                <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                    Our all encompassing School Management Portal is the only solution needed for any institution.
-                                    The School Management Portal (SMP) is a tool that can help educational institutions of all kinds manage their administrative tasks, automate processes, and streamline communication between teachers, students, parents, and administrators. 
-                                    The software can be used to manage all aspects of school operations, including student enrollment, attendance, grades, schedules, and more.
-                                </p>
-                                <div className="flex justify-center my-4">
-                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
-                                        Schedule Demo
-                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                    </Link>
-                                </div>
-                                
-                                {/* <ul className="border-t border-gray-300 py-8 space-y-6">
-                                    <li className="flex items-center space-x-2 px-8">
-                                        <span className="bg-blue-600 rounded-full p-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                                            </svg>
-                                        </span>
-                                        <span className="text-gray-600 capitalize">Free Setup Guidance</span>
-                                    </li>
-                                </ul> */}
-                            </div>
-                        </div>
-
-                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
-                            <div className="m-2 text-justify text-sm">
-                            <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Payroll Management System</h4>
-                                <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                    The Payroll Management System automates the process of employee payments for businesses and organizations of different sizes. It helps to ensure that your employees get paid accurately and on time, while also reducing the time and effort required to manage payroll manually, with this system in place, you enjoy benefits such as: 
-                                    increased accuracy in employee payments. It can also integrate into an existing management system through well-structured APIs.
-                                </p>
-                                <div className="flex justify-center my-4">
-                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
-                                        Schedule Demo
-                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
-                            <div className="m-2 text-justify text-sm">
-                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Event Management System</h4>
-                                <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                    Our event management system helps to manage different types of events, whether weddings, burials or any kind of event. With an easily customizable menu, you get to define your user experience and make your event fully customizable and memorable.
-                                    With use cases already recorded for quite a number of events, we can assure you of a stress-less event mangement platform.
-                                </p>
-                                <div className="flex justify-center my-4">
-                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
-                                        Schedule Demo
-                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>                    
-                    </div>
-                </div>
+              </div>
             </div>
-        </>
-    )
-}
+
+            <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+              <div className="m-2 text-justify text-sm">
+                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
+                  Social Gatherings{" "}
+                </h4>
+                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                  Once a month we hold Mixed socials with the brothers and
+                  sisters, this social consit of hiking, apple picking, DC trip,
+                  etc. We also have brothers socials and sisters social.
+                   Join the group me below for more information
+                </p>
+                <div className="flex justify-center my-4">
+                  <Link
+                    to="/get-demo"
+                    className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl"
+                  >
+                    One Love,One Msa
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="https://groupme.com/join_group/16797770/ZBprRX"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+              <div className="m-2 text-justify text-sm">
+                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
+                  Donations
+                </h4>
+                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                  Support our mission to promote the goals of the MSA by contributing to our fundraising campaign. Your generous
+                  donations enable us to organize events and initiatives that foster community engagement. no
+                  matter the size, makes a significant impact in advancing our
+                  shared objectives.
+                </p>
+                <div className="flex justify-center my-4">
+                  <Link to="/get-demo" className="text-black bg-Green-500 hover:bg-green-500 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                          cash App : $msajmu
+                        </Link>
+                </div>
+                <div className="flex justify-center my-4">
+                  <Link to="/get-demo" className="text-black bg-Blue-500 hover:bg-blue-500 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                          Venmo : Jmu_msa
+                        </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+              <div className="m-2 text-justify text-sm">
+                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
+                  Banquets
+                </h4>
+                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                  Celebrate the spirit of Eid with us as we host vibrant
+                  banquets at the beginning and end of the holy month. Join our
+                  charity Banquets, where we come together to support those in
+                  need and make a positive impact in our community.
+                </p>
+                <div className="flex justify-center my-4">
+                  <Link
+                    to="/get-demo"
+                    className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl"
+                  >
+                    Banquets
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Portfolio;
