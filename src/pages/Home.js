d41dd9133798a46caf9b96e3e82c26c743751757
Naweed Yarzada <yarzadnx@dukes.jmu.edu>
2024-05-05 19:18:11 -0400

    removedstuff that wasnt working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import Clients from '../components/partners';
-import Cta from '../components/Cta';
-import Footer from '../components/Footer';
-import Hero from '../components/header';
-import Intro from '../components/introduction';
-import Portfolio from '../components/Portfolio';
-import Services from '../components/quran_and-dua';
-
+import React from "react";
+import Clients from "../components/partners";
+import Cta from "../components/Cta";
+import Footer from "../components/Footer";
+import Hero from "../components/header";
+import Intro from "../components/introduction";
+import Portfolio from "../components/Portfolio";
+import Services from "../components/quran_and-dua";
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-            <Intro />
-            <Services />
-            <Portfolio />
-            <Clients />
-            <Cta/>
-            <Footer />
-        </>
-
-    )
-}
+  return (
+    <>
+      <Hero />
+      <Intro />
+      <Services />
+      <Portfolio />
+      <Clients />
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
-
